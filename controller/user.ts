149d6a9fd8a64eb82bad5fb913e086dbc03d9a8e
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-03-25 01:20:13 +0530

    update

--- controller/user.ts
+++ controller/user.ts
@@ -1,6 +1,6 @@
 //@ts-ignore
 import User from '../model/user'
-
+import jwt from 'jsonwebtoken'
 export default interface usertype {
     name: string
     email: string
@@ -13,8 +13,16 @@ const newuser = async (payload: usertype)=> {
     try {
 
         const {name , email} = payload;
+       const exists = await User.findOne({email: email});
+       if(exists){
+        return exists;
+       }
+       else{
         const data = await User.create({name, email});
         return data;
+
+       }
+        
 
         
     } catch (error) {
@@ -22,4 +30,10 @@ const newuser = async (payload: usertype)=> {
     }
 }
 
-module.exports = {newuser}
+
+const validateuser = async ()=>{
+    const check = jwt.verify("AMf-vBzErHLUSsfGPs-3PTjX1BHI3JzQZQDcbJFz57Cpi0p6FMKwEyF5cxv55huFQ4AQjBZb89q0iZUK3jAg2DGDH-U72oNN1uO5a_zjQ1mB3rgXxCR99xF4ifEVTaXHnut9BMBoPwMfgDfcEu07ncSBjsdIxxbJ-D48J0P3GJa1yeAx68sKUV-yZNdkFHh_o0fSt95wSFKtG_b-P7iUY8IT6_yRLkAupUH4qR-nGg7U6_T5qiCqLQZ13Q0-wq5VfjBxcC7r3QGO0IZpnqlLQ9UR9E9NLDWAucPbOLGEa3g5mi_J7fjK8dw84afm90HTGAjL0t5T0_uLZwcQTokBXSiUa8f2ga2Z53t-HwTphzJPt0uPKxXxoTKUP6eSxoUdCdxy3aGfGwOHkpwr4dVNreoxDE3pXHZnKNeZw2tVBikTXGSm7ChuZ2Xz8uVlsS7RtAgB5Hrbf4UJ","shh");
+    console.log(check)
+}
+
+module.exports = {newuser, validateuser}
--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import cors from "cors";
 import dotenv from 'dotenv'
 import morgan from "morgan"
 dotenv.config();
+import jwt from 'jsonwebtoken'
 
 async function init () {
     const app: Express = express ()
@@ -31,6 +32,18 @@ app.use('/api', apirouter);
 
 app.get('/', async (req: Request, res:Response)=>{
     res.json({message: "Server Running"})
+    
+        // const check = jwt.verify("eyJhbGciOiJSUzI1NiIsImtpZCI6ImViYzIwNzkzNTQ1NzExODNkNzFjZWJlZDI5YzU1YmVmMjdhZDJjY2IiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiVmlkaXQgVGFtcmFrYXIiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS1h5di1aSVYydExrTXY4cEwxb2s0OG1HZVJJMWMzSi1mbmtDVDBaY3hHPXM5Ni1jIiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2VsZWN0cmljYXJ0LWRkMjY1IiwiYXVkIjoiZWxlY3RyaWNhcnQtZGQyNjUiLCJhdXRoX3RpbWUiOjE3MTEzMDk1MjUsInVzZXJfaWQiOiJyalozQVVmcE5FWXJZNmxOVHZLWTFOMGpwc0ozIiwic3ViIjoicmpaM0FVZnBORVlyWTZsTlR2S1kxTjBqcHNKMyIsImlhdCI6MTcxMTMwOTUyNSwiZXhwIjoxNzExMzEzMTI1LCJlbWFpbCI6InZpZGl0LnRhbXJha2FyMTZAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMDg2NDMxMjIyNzgwNDYwODQ5MDkiXSwiZW1haWwiOlsidmlkaXQudGFtcmFrYXIxNkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.uBeJnSMbfPbUGdbqhnSQvAWPtGUczigfuH2dvtrmaCoBxs1xME_4Su5nYER2aXcDDC58RqBldm0vD2isOS7DuUNj5_LVwcycm7T6I5AmGvV2R8SLS9m2btoMw-C_Fvc8s9wrL8UracyGiI3gk1obpRGPrJIPw_78ObilmK6JG-4qQsiAR74CsHduZEUJgvrU7llf8AD9jO6fY8aGMXlP7xqmLjwnmyRiUdY5J3X-7Y-1CrWBxoft4Aq4uVZCcEBlLnpiQsEFNhhb6kwWZJCaiJsPOlYNnUnvJ1MtGhaNOiYbTqgZWoNlKtAXLwa-MDspeJmmWtyaTrUDy-lbEQrF1w"
+        // ,"shh", function(err, decoded) {
+        //     if (err) {
+        //         return res.json({
+        //             success: false,
+        //             message: "Failed to authenticate token.",
+        //         });
+        //     } });
+        // console.log(check)
+        
+   
 })
 
 app.listen (8000, ()=>{
